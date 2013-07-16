b15dcb69d64af1ddc00338e29f682cdaef36691c
Milan Kolar <m.kolar@me.com>
2013-07-16 16:22:30 +0100

    Remove Dev area

--- tank_dev/install/apps/tk-multi-workfiles/v0.2.5/README.md
+++
@@ -1,2 +0,0 @@
-tk-multi-workfiles
-==================
--- tank_dev/install/apps/tk-multi-workfiles/v0.3.3/python/tk_multi_workfiles/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Copyright (c) 2013 Shotgun Software, Inc
-----------------------------------------------------
-
-"""
-
-from .work_files import WorkFiles
-from .versioning import Versioning
-from .save_as import SaveAs
